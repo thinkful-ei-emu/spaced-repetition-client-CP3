6a863f66b377e3dfa00b57630abc6e184edb39d7
Paul Han <kyutae.han93@gmail.com>
2019-09-09 17:23:13 -0400

    setting up /learn

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react'
+import './LearningRoute.css';
 import {Link} from 'react-router-dom';
 import SRContext from '../../contexts/SRContext';
+import LanguageApiService from '../../services/language-api-service';
 
 class LearningRoute extends Component {
+  state={
+    isInQuestion:true,
+    currentQ:{},
+    guess:''
+  }
+  getNewHead=()=>{
+    LanguageApiService.getHead()
+      .then(res=>{
+        this.setState({
+          currentQ:res,
+          isInQuestion:true
+        })
+      })
+  }
+
+  handleGuess=(guess)=>{
+    this.setState({guess})
+  }
+
+  submitGuess=()=>{
+    LanguageApiService.postGuess(this.state.guess)
+      .then(res=>{
+
+      })
+      .then(()=>{
+        this.setState({
+          isInQuestion:false,
+        })
+      })
+  }
+
+  componentDidMount(){
+    this.getNewHead();
+  }
   static contextType = SRContext
   render() {
     return (
-      <section>
-        implement and style me
+      <section className='learnWordSection'>
+        <h3>
+          {this.state.currentQ.original &&
+            this.state.currentQ.original
+          }
+        </h3>
+        <label htmlFor='learn-guess-input'>Guess: </label>
+        <input id='learn-guess-input' value={this.state.guess} onChange={e=>{this.handleGuess(e.target.value)}}/>
+        <button onSubmit={e=>{
+          e.preventDefault();
+          this.submitGuess();
+        }}>Submit</button>
       </section>
     );
   }
