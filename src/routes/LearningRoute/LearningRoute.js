729e159095647bfffcb8536fe7064b6738330179
Paul Han <kyutae.han93@gmail.com>
2019-09-10 11:09:56 -0400

    almost have next

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,59 +1,101 @@
 import React, { Component } from 'react'
 import './LearningRoute.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import SRContext from '../../contexts/SRContext';
 import LanguageApiService from '../../services/language-api-service';
 
 class LearningRoute extends Component {
-  state={
-    isInQuestion:true,
-    currentQ:{},
-    guess:''
+  state = {
+    isInQuestion: true,
+    currentQ: {},
+    nextQ: {},
+    guess: '',
+    answer: '',
+    error: ''
   }
-  getNewHead=()=>{
-    LanguageApiService.getHead()
-      .then(res=>{
+  getNewHead = () => {
+    return LanguageApiService.getHead()
+      .then(res => {
         this.setState({
-          currentQ:res,
-          isInQuestion:true
+          currentQ: res,
+          isInQuestion: true
         })
       })
   }
 
-  handleGuess=(guess)=>{
-    this.setState({guess})
+  handleGuess = (guess) => {
+    this.setState({ guess })
   }
 
-  submitGuess=()=>{
+  submitGuess = () => {
     LanguageApiService.postGuess(this.state.guess)
-      .then(res=>{
-
-      })
-      .then(()=>{
+      .then(res => {
         this.setState({
-          isInQuestion:false,
+          isInQuestion: false,
+          currentQ: res,
+          answer: res.answer,
+          nextQ: { ...res }
         })
       })
+      .catch(e => this.setState({ error: e, guess: '', answer: '' }))
   }
 
-  componentDidMount(){
+  moveToNextWord=()=>{
+
+  }
+
+  componentDidMount() {
     this.getNewHead();
   }
   static contextType = SRContext
   render() {
     return (
       <section className='learnWordSection'>
-        <h3>
-          {this.state.currentQ.original &&
-            this.state.currentQ.original
-          }
-        </h3>
-        <label htmlFor='learn-guess-input'>Guess: </label>
-        <input id='learn-guess-input' value={this.state.guess} onChange={e=>{this.handleGuess(e.target.value)}}/>
-        <button onSubmit={e=>{
-          e.preventDefault();
-          this.submitGuess();
-        }}>Submit</button>
+        {this.state.isInQuestion ?
+
+          <>
+            <h2>Translate the word:</h2>
+            <h3>
+              {this.state.currentQ.nextWord &&
+                this.state.currentQ.nextWord
+              }
+            </h3>
+            <label htmlFor='learn-guess-input'>Guess: </label>
+            <input id='learn-guess-input' value={this.state.guess} onChange={e => { this.handleGuess(e.target.value) }} />
+            <br></br>
+            <button className='learn-submit-guess-button' onClick={e => {
+              this.submitGuess();
+            }}>Submit</button>
+          </>
+          :
+          (
+            (this.state.answer === this.state.guess) ?
+              <>
+                <h2>You were correct! :D</h2>
+                <p>{`The correct translation for ${this.state.currentQ.nextWord} was ${this.state.answer} and you chose ${this.state.guess}`}</p>
+                <button onClick={e => {
+                  this.moveToNextWord();
+                }}>
+                  Try another word
+                </button>
+                <p>Your total score is: {this.state.nextQ.totalScore}</p>
+              </>
+              :
+              <>
+                <h2>Good try, but not quite right :(</h2>
+                <p>{`The correct translation for ${this.state.currentQ.nextWord} was ${this.state.answer} and you chose ${this.state.guess}`}</p>
+                <button onClick={e => {
+                  this.moveToNextWord();
+                }}>
+                  Try another word
+              </button>
+                <p>Your total score is: {this.state.nextQ.totalScore}</p>
+              </>
+          )
+
+
+        }
+
       </section>
     );
   }
